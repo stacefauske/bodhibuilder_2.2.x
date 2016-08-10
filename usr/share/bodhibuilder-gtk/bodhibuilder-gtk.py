ff9a13a03cf51cc6fe24471cbf5a5187836bc956
stacefauske <stace.fauske@gmail.com>
2016-08-09 21:58:07 -0400

    Update version. Update to newer files from 16.04 repos.

--- usr/share/bodhibuilder-gtk/bodhibuilder-gtk.py
+++ usr/share/bodhibuilder-gtk/bodhibuilder-gtk.py
@@ -63,7 +63,7 @@ except:
 
 APP = "bodhibuilder"
 DIR = "/usr/share/locale"
-APP_VERSION = "2.2.beta-05"
+APP_VERSION = "2.2.beta-06"
 
 locale.setlocale(locale.LC_ALL, '')
 gettext.bindtextdomain(APP, DIR)
