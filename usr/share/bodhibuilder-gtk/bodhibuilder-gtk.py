e992d6485ea301c22c012cf17da95ecda2873ee0
stacefauske <stace.fauske@gmail.com>
2016-09-10 21:56:24 -0400

    added new deb files for offline install, separated into 32 & 64 bit package sets

--- usr/share/bodhibuilder-gtk/bodhibuilder-gtk.py
+++ usr/share/bodhibuilder-gtk/bodhibuilder-gtk.py
@@ -63,7 +63,7 @@ except:
 
 APP = "bodhibuilder"
 DIR = "/usr/share/locale"
-APP_VERSION = "2.2.beta-06"
+APP_VERSION = "2.2.beta-07"
 
 locale.setlocale(locale.LC_ALL, '')
 gettext.bindtextdomain(APP, DIR)
@@ -71,6 +71,7 @@ gettext.textdomain(APP)
 _ = gettext.gettext
 
 LOCALE = locale.getlocale()[0]
+
 
 class appgui:
     def __init__(self):
